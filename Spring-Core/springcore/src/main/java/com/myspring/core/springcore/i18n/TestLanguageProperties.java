c368ffc84a30a1928138c6279ddf65bcd1a88188
johnyshaik <jbshaik82@gmail.com>
2018-03-06 21:42:34 -0500

    Spring-Core: Test File
    	Testing related to setting the properties of the bean, read from the external properties file.

---
+++ Spring-Core/springcore/src/main/java/com/myspring/core/springcore/i18n/TestLanguageProperties.java
@@ -0,0 +1,32 @@
+package com.myspring.core.springcore.i18n;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author johnybasha
+ *
+ */
+public class TestLanguageProperties {
+
+	/**
+	 * Default Constructor.
+	 */
+	public TestLanguageProperties() {
+		super();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AbstractApplicationContext appContext = new ClassPathXmlApplicationContext(
+				"com/myspring/core/springcore/i18n/language-config.xml");
+
+		Welcome welcome = (Welcome) appContext.getBean("welcome");
+		System.out.println(welcome);
+
+		appContext.registerShutdownHook();
+		appContext.close();
+	}
+}
