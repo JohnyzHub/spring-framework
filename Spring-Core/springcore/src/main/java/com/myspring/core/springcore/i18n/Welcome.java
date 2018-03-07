261979805a682674cee5fc23f78032e23852567f
johnyshaik <jbshaik82@gmail.com>
2018-03-06 21:43:15 -0500

    Spring-Core: POJO File
    	POJO file with welcome greeting attributes.

---
+++ Spring-Core/springcore/src/main/java/com/myspring/core/springcore/i18n/Welcome.java
@@ -0,0 +1,34 @@
+package com.myspring.core.springcore.i18n;
+
+/**
+ * @author johnybasha
+ *
+ */
+public class Welcome {
+
+	private String greetings;
+
+	/**
+	 * Default Constructor
+	 */
+	public Welcome() {
+		super();
+	}
+
+	public Welcome(String greetings) {
+		this.greetings = greetings;
+	}
+
+	public String getGreetings() {
+		return greetings;
+	}
+
+	public void setGreetings(String greetings) {
+		this.greetings = greetings;
+	}
+
+	@Override
+	public String toString() {
+		return "Welcome [greetings=" + greetings + "]";
+	}
+}
